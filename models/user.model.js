5c721e14afce6ee1edd255f2cedf3f0d98cc1fc0
Paulo Roberto da Silva <paulorob_s@hotmail.com>
2016-01-11 05:00:45 -0200

    modificação no model

--- models/user.model.js
+++ models/user.model.js
@@ -2,11 +2,11 @@ var db = require('../config/db.config.js');
 
 
 exports.save = function(usuario, callback){
-    db.query('INSERT INTO usuarios set ?', usuario, function(err, resultado){
+    db.query('INSERT INTO usuarios set ?', usuario, function(err, rows, fields){
        if(err){
            callback({error: err});
-       } else if(resultado){         
-           callback(resultado);
+       } else if(rows){         
+           callback({message:'Cadastrado com sucesso'});
        }else{          
            callback({message:'Algo aconteceu de errado'});
        }
