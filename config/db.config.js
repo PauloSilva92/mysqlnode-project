94015366a48c96c64d8afd66399d82443fc7c455
Paulo Roberto da Silva <paulorob_s@hotmail.com>
2016-01-11 03:10:50 -0200

    db.config.js

---
+++ config/db.config.js
@@ -0,0 +1,12 @@
+var mysql =  require('mysql');
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'mysqlnode', //mude esse campo para o usuario que seu banco utiliza
+    password: 'MysqlNode1!', //mude esse campo para a senha do seu usuario do banco de dados
+    database: 'mysqlnode' //mude esse campo para o nome do seu banco de dados
+});
+
+connection.connect();
+
+module.exports = connection;
